90e146409d08df6e76a81a6695a2cc6be1386f47
GERMAN OLIVEROS ACOSTA <129201492+OLIVEROS99@users.noreply.github.com>
2025-02-22 16:46:59 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,12 +2,9 @@ const evento = document.getElementById('send')
 const enviarFormulario =() => {
         let nombre = document.getElementById('nombres').value;
         let apellido = document.getElementById('apellidos').value;
-        let cedula = document.getElementById('cedula').value;
-        let phone = document.getElementById('telefono').value;
-        let correo = document.getElementById('correo').value;
         let mensaje = document.getElementById('mensaje').value;
         let numero= 573146597922;
 var win= window.open(`https://wa.me/${numero}?text=Hola%20mi%20nombre%20es%20${nombre}
-%20${cedula}, Asunto:%20${mensaje}`,'_blank');       
+%20${apellido}, Asunto:%20${mensaje}`,'_blank');       
 }
 evento.addEventListener('click', enviarFormulario)
